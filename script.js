a6a3bdd053f848ee608ae6d7deed8346b99a58e9
Dilkhush Kumar Thakur <154700766+dilkhush05@users.noreply.github.com>
2024-06-22 22:29:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// script.js
+
+document.getElementById('signupForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Thank you for subscribing!');
+    // Here you can also add code to handle the form data if needed.
+});
